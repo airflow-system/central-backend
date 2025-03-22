db3287d8317bb3239b3d69a0f636a605c01ef69e
Dhruv-86 <dhruvrpatel86@gmail.com>
2025-03-22 13:41:27 -0500

    Initial central backend commit

---
+++ src/test/java/com/airflow/centralbackend/CentralBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airflow.centralbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
